9708bba233b9a84018edff03e0626abdf1d9f528
Mateus Cruz <m.cruz.rossetto@gmail.com>
2021-12-14 22:34:35 -0300

    buscando filmes API e mudança de tela

--- src/Components/ScreenFilms/ScreenFilms.js
+++ src/Components/ScreenFilms/ScreenFilms.js
@@ -1,64 +1,27 @@
+import { Link } from "react-router-dom";
+
 import Header from '../Header/Header';
 import TituleSection from '../TitleSection/TitleSection';
 import './style.css';
 
-import kinoplex from '../../assets/kinoplex.jpeg';
-import fastFurious_9 from '../../assets/fastFurious_9.jpeg';
+export default function ScreenFilms ({ films }){
 
+    console.log(films);
 
-export default function ScreenFilms (){
     return(
         <div className="screenFilms">
             <Header />
             <main>
                 <TituleSection text={'Selecione o filme'} />
-                
-                <div className="colun">
-                    <div className="films">
-                        <div className="film">
-                            <img src={kinoplex} alt='film' />
-                        </div>
 
-                        <div className="film">
-                            <img src={kinoplex} alt='film' />
-                        </div>
-
-                        <div className="film">
-                            <img src={kinoplex} alt='film' />
-                        </div>
-
-                        <div className="film">
-                            <img src={kinoplex} alt='film' />
-                        </div>
-
-                        <div className="film">
-                            <img src={kinoplex} alt='film' />
-                        </div>
-
-                    </div>
-
-                    <div className="films">
-
-                        <div className="film ">
-                            <img src={fastFurious_9} alt='film' />
-                        </div>
-
-                        <div className="film">
-                            <img src={fastFurious_9} alt='film' />
-                        </div>
-
-                        <div className="film">
-                            <img src={fastFurious_9} alt='film' />
-                        </div>
-
-                        <div className="film">
-                            <img src={fastFurious_9} alt='film' />
-                        </div>
-
-                        <div className="film">
-                            <img src={fastFurious_9} alt='film' />
-                        </div>
-                    </div>
+                <div className="films">
+                    {films.map( (film) => (
+                        <Link to='/times'>
+                            <div key={film.id} className="film">
+                                <img src={film.posterURL} alt='film' />
+                            </div>
+                        </Link>
+                    ))}
                 </div>
 
             </main>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import ReactDom from 'react-dom';
+import { render } from 'react-dom';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import axios from 'axios';
+
 import ScreenFilms from './Components/ScreenFilms/ScreenFilms';
 import ScreenTimes from './Components/ScreenTimes/ScreenTimes';
 import ScreenSeats from './Components/ScreenSeats/ScreenSeats';
@@ -8,11 +12,26 @@ import './reset.css';
 import './style.css';
 
 export default function App(){
-    return (
-        //<ScreenFilms />
-        //<ScreenTimes />
-        //<ScreenSeats />
-        <ScreenFinished />
-    )}
 
-ReactDom.render(<App />, document.querySelector(".root"));
+    const [films, setFilms] = useState([]);
+
+    useEffect(() => {
+        const promise = axios.get('https://mock-api.driven.com.br/api/v4/cineflex/movies');
+        promise.then( elements => {
+            setFilms(elements.data);
+        })
+    }, []);
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<ScreenFilms films={films}/>} />
+                <Route path="/times" element={<ScreenTimes />} />
+                <Route path="/times" element={<ScreenSeats />} />
+                <Route path="/times" element={<ScreenFinished />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+render(<App />, document.querySelector(".root"));
